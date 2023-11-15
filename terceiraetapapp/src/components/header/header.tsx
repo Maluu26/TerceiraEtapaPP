106db87157d7d0c3a8601f8ad6301db6cf5f165f
Kaio Lima <kaionascimentolima12323@gmail.com>
2023-11-15 18:41:43 -0300

    chore: add EJ's name under user name in header

--- terceiraetapapp/src/components/header/header.tsx
+++ terceiraetapapp/src/components/header/header.tsx
@@ -1,19 +1,22 @@
-import './header.css';
+import "./header.css";
 
-interface HeaderProps{
-    avatarPic: string,
-    encejLogo: string
+interface HeaderProps {
+  avatarPic: string;
+  encejLogo: string;
 }
-export default function Header({avatarPic, encejLogo} : HeaderProps){
-    return(
-        <div className="inicio">
-        <div className=" img-avatar-div">
-            <img src={avatarPic}className="avatar-pic" alt="" />
-            <h1 className="hello">Olá, Fulano</h1>
+export default function Header({ avatarPic, encejLogo }: HeaderProps) {
+  return (
+    <div className="inicio">
+      <div className=" img-avatar-div">
+        <img src={avatarPic} className="avatar-pic" alt="" />
+        <div>
+          <h1 className="hello">Olá, Fulano</h1>
+          <p className="hello-ej">CT Júnior</p>
         </div>
-        <div className="encej">
-            <img src={encejLogo} className="encej-logo" alt="" />
-        </div>
+      </div>
+      <div className="encej">
+        <img src={encejLogo} className="encej-logo" alt="" />
+      </div>
     </div>
-    );
+  );
 }
